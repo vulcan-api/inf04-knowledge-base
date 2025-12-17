4b651e2410552f52868736a45698fc63cd4f7702
kujawiak06 <kubaolejnik2006@gmail.com>
2025-12-17 22:19:06 +0100

    arkusze lato25

---
+++ egzaminy/INF04CezarK/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+class Program
+{
+        public static string Szyfruj(string tekst, int klucz)
+        {
+            klucz = klucz % 26; //klucz z przedziału 0 do 25
+            char[] wynik = new char[tekst.Length]; 
+
+            for (int i = 0; i < tekst.Length; i++)
+            {
+                char znak = tekst[i];   //rozbicie napisu na "tablice"
+
+                if (znak >= 'a' && znak <= 'z')
+                {
+                    wynik[i] = (char)((znak - 'a' + klucz + 26) % 26 + 'a');
+                }//przesunięcie o daną ilośc z klucza
+                else
+                {
+                    wynik[i] = znak;
+                }
+            }
+
+            return new string(wynik);
+        }
+
+
+    static void Main()
+    {
+        Console.WriteLine("Podaj napis do zaszyfrowania:");
+        string tekst = Console.ReadLine();      //zaczytanie napisu
+
+        Console.WriteLine("Podaj klucz szyfrowania:");
+        int klucz = Convert.ToInt32(Console.ReadLine());    //zaczytanie i przekonwertowanie klucza
+
+        Console.WriteLine(Szyfruj(tekst, klucz));
+    }
+}
